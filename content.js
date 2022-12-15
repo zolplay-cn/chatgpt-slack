599a9b63a7fad89a332bc9d31515706c2886df91
Cali Castle <hi@cali.so>
2022-12-15 11:43:04 +0800

    fix some stuff

---
+++ content.js
@@ -0,0 +1,103 @@
+const TEXTAREA_SELECTOR = '#__next main form textarea[data-id]';
+
+// set up a queue to handle messages from the server
+/**
+ * @type {{text: string; ts: string; pending_ts: string; channel: string}[]}
+ */
+const queue = [];
+/** @type {WebSocket|null} */
+let socket;
+let isWaitingForResponse = false;
+
+function enterPromptAndWaitForResponse(payload, onResponded = (payload) => {}) {
+  const textarea = document.querySelector(TEXTAREA_SELECTOR);
+  const submitButton = document.querySelector(`${TEXTAREA_SELECTOR} + button`);
+  textarea.value = payload.text;
+
+  setTimeout(() => {
+    submitButton.click();
+
+    const observer = new MutationObserver((mutations) => {
+      mutations.forEach((mutation) => {
+        const dataId = document.querySelector(TEXTAREA_SELECTOR).getAttribute('data-id');
+        const container = mutation.target.querySelector(`div[class^='${dataId}']`);
+        const buttons = container?.parentNode?.parentNode?.querySelectorAll('button');
+        if (container && buttons && buttons.length === 2) {
+          observer.disconnect();
+          onResponded({
+            ...payload,
+            text: container.innerText,
+          });
+        }
+      });
+    });
+    observer.observe(document.querySelector("#__next main div[class^='react-scroll-to-bottom-']"), {
+      subtree: true,
+      childList: true,
+    });
+  }, 300);
+}
+
+/**
+ * Send the response back to the WebSocket server.
+ *
+ * @param {object} payload
+ * @returns
+ */
+function sendResponse(payload) {
+  if (!payload) return;
+
+  socket?.send(JSON.stringify({ type: 'response', payload }));
+}
+
+function simulate() {
+  const nextPacket = queue.shift();
+  enterPromptAndWaitForResponse(nextPacket, (payload) => {
+    sendResponse(payload);
+    setTimeout(() => {
+      isWaitingForResponse = false;
+    }, 100);
+  });
+}
+
+function processQueue(timeout = 500) {
+  if (queue.length > 0 && !isWaitingForResponse) {
+    isWaitingForResponse = true;
+    simulate();
+  }
+
+  setTimeout(() => processQueue(timeout), timeout);
+
+  // check if the session has expired
+  document.body.querySelectorAll('[data-headlessui-portal]').forEach((div) => {
+    if (div?.innerHTML?.includes('Your session has expired')) {
+      socket?.send(JSON.stringify({ type: 'session_expired' }));
+    }
+  });
+}
+
+window.onload = () => {
+  if (window.location.pathname !== '/chat') return;
+
+  socket = new WebSocket('ws://localhost:3001');
+
+  // receive a message from the server
+  socket.addEventListener('message', ({ data }) => {
+    const packet = JSON.parse(data);
+
+    switch (packet.type) {
+      case 'prompt':
+        queue.push(packet.payload);
+
+        break;
+    }
+  });
+
+  // process queue items every 0.5 seconds
+  processQueue(500);
+
+  // refresh the page in 30 minutes
+  setTimeout(() => {
+    window.location.reload();
+  }, 30 * 60 * 1000);
+};
--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,6 @@
-import './utils/env'
-import { App, LogLevel } from '@slack/bolt'
-import { Server as WebSocketServer } from 'ws'
+import './utils/env';
+import { App, LogLevel } from '@slack/bolt';
+import { Server as WebSocketServer } from 'ws';
 
 const app = new App({
   token: process.env.SLACK_BOT_TOKEN,
@@ -8,37 +8,52 @@ const app = new App({
   signingSecret: process.env.SLACK_SIGNING_SECRET,
   logLevel: LogLevel.INFO,
   socketMode: true,
-})
+});
 
-function sendStatusCheckMessage(connected: boolean) {
+const notifiee = process.env.STATUS_CHECK_SLACK_MEMBER as string;
+
+function sendStatusCheckMessage(connected: boolean | string) {
   return app.client.chat.postMessage({
     token: app.client.token as string,
-    channel: process.env.STATUS_CHECK_SLACK_MEMBER as string,
-    text: `ChatGPT WebSocket ${connected ? 'connected :white_check_mark:' : 'disconnected :x:'}`,
-  })
+    channel: notifiee,
+    text:
+      typeof connected === 'boolean'
+        ? `ChatGPT WebSocket ${connected ? 'connected :white_check_mark:' : 'disconnected :x:'}`
+        : connected,
+  });
 }
 
-const ws = new WebSocketServer({ port: parseInt(process.env.WS_PORT || '3001') })
+const ws = new WebSocketServer({ port: parseInt(process.env.WS_PORT || '3001') });
 
-type Payload = { text: string; ts: string; thread_ts?: string; channel: string }
+type Payload = { text: string; ts: string; thread_ts?: string; channel: string };
+
+// only send one message when the websocket is disconnected
+let sessionExpired = false;
 
 ws.on('connection', (socket) => {
-  sendStatusCheckMessage(true)
+  sendStatusCheckMessage(true);
 
   socket.on('message', (message) => {
-    const packet = JSON.parse(message.toString())
+    const packet = JSON.parse(message.toString());
 
     switch (packet.type) {
       case 'response':
-        handleWSResponse(packet.payload)
-        return
+        handleWSResponse(packet.payload);
+        return;
+      case 'session_expired':
+        if (sessionExpired) return;
+
+        sendStatusCheckMessage('ChatGPT session expired :x:');
+
+        sessionExpired = true;
+        return;
     }
-  })
-})
+  });
+});
 
 ws.on('close', () => {
-  sendStatusCheckMessage(false)
-})
+  sendStatusCheckMessage(false);
+});
 
 function sendWSPrompt(payload: Payload) {
   ws.clients.forEach((client) => {
@@ -47,8 +62,8 @@ function sendWSPrompt(payload: Payload) {
         type: 'prompt',
         payload,
       })
-    )
-  })
+    );
+  });
 }
 
 async function handleWSResponse(response: Payload) {
@@ -57,54 +72,59 @@ async function handleWSResponse(response: Payload) {
     channel: response.channel,
     thread_ts: response.thread_ts ?? response.ts,
     text: response.text,
-  })
+  });
 }
 
 app.use(async ({ next }) => {
-  await next()
-})
+  await next();
+});
 
 const whitelistedChannels = [
   // #chatgpt-test for Cali's testing
   'C04EXK6T85U',
   // #chatgpt-playground for the public
   'C04F7ML901G',
-]
+];
 
 // Listens to incoming messages that contain "hello"
-app.message(/^q\?/, async ({ message }) => {
+app.message(/^q\?/, async ({ message, say }) => {
   // Filter out messages from channels that are not whitelisted
   if (!whitelistedChannels.includes(message.channel)) {
-    return
+    return;
   }
 
   // Filter out message events with subtypes (see https://api.slack.com/events/message)
   if (message.subtype === undefined || message.subtype === 'bot_message') {
-    const { text, ts, channel, thread_ts } = message
+    const { text, ts, channel, thread_ts } = message;
     // check if the message is a valid string
-    if (typeof text !== 'string') {
-      return
+    if (typeof text !== 'string') return;
+
+    const prompt = text?.replace(/^q\?/, '').trim();
+
+    if (prompt.length === 0) return;
+
+    if (sessionExpired) {
+      await say({
+        text: `ChatGPT session expired, need to re-login in browser. /cc <@${notifiee}>`,
+        thread_ts: thread_ts ?? ts,
+        channel,
+      });
+      return;
     }
 
-    const prompt = text?.replace(/^q\?/, '').trim()
-
-    if (prompt.length === 0) {
-      return
-    }
-
-    sendWSPrompt({ text: prompt, ts, thread_ts, channel })
+    sendWSPrompt({ text: prompt, ts, thread_ts, channel });
 
     await app.client.reactions.add({
       token: app.client.token as string,
       name: 'typingcat',
       channel,
       timestamp: ts,
-    })
+    });
   }
-})
-;(async () => {
+});
+(async () => {
   // Start your app
-  await app.start(Number(process.env.PORT) || 3000)
+  await app.start(Number(process.env.PORT) || 3000);
 
-  console.log('⚡️ Bolt app is running!')
-})()
+  console.log('⚡️ Bolt app is running!');
+})();
